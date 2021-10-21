516734350c17feaced3a873d202efeb1e297b8f4
Mahmoud Fouad Ali Abo Ali <mfaworkstation@gmail.com>
2021-10-21 05:18:13 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -8,20 +8,13 @@ import { DISHES } from './shared/dishes'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES
-    };
+    
   }
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Safari Coffee and Restaurant</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
-      </div>
+      <> 
+      
+      </>
     );
   }
 }
